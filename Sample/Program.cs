4b86cca1b1b404b98e33e155981a10cc03247fa8
Hamster CheeseAllergy <shlifedev@gmail.com>
2022-10-27 11:42:12 +0900

    sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
-using CAH.TypeChecker;
 using System.Collections.Generic;
 using System.Linq;
+using CAH.TypeChecker;
 namespace Sample
 {
+
+  public enum GameState
+  {
+    Playing,
+    End,
+    Something
+  }
+  public class CustomEnum : EnumType<GameState>
+  {
+    public override List<string> TypeDeclarations => new List<string>() { "GameState" };
+  }
+
   public struct Vector3
   {
     public float x, y, z;
@@ -18,8 +30,7 @@ namespace Sample
     }
   }
 
-
-  public class Vector3Type : IType<Vector3>
+    public class Vector3Type : IType<Vector3>
   {
     public List<string> TypeDeclarations => new List<string>() { "Vector3", "Vec3" };
     public Vector3 Read(string value)
@@ -33,6 +44,8 @@ namespace Sample
       return $"${value.x},{value.y},{value.z}";
     }
   }
+
+  
   class Program
   {
     static void Main(string[] args)
@@ -41,11 +54,18 @@ namespace Sample
       var vector3 = checker[typeof(Vector3)].Read("1,2,3");
       var integer = checker[typeof(int)].Read("100000");
       var floating = checker[typeof(float)].Read("25.6123");
+      var enumTest = checker[typeof(GameState)].Read("End");
+      var enumTest2 = checker["GameState"].Read("Playing");
 
-
-      Console.WriteLine(vector3.ToString());
+  
+      Console.WriteLine("-- primitive --");
       Console.WriteLine(integer);
       Console.WriteLine(floating);
+      Console.WriteLine("-- custom vector3 type --");
+      Console.WriteLine(vector3.ToString());
+      Console.WriteLine("-- enum --");
+      Console.WriteLine(enumTest);
+      Console.WriteLine(enumTest2);
 
       while (true) { }
     }
